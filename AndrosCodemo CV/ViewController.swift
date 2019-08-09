8c1f18b13e32d21b0e23b10c09d447b70064ce85
Andros Codemo <androsodemo@live.com.mx>
2019-08-09 16:47:17 -0500

    Initial Commit

---
+++ AndrosCodemo CV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AndrosCodemo CV
+//
+//  Created by Andros Codemo on 8/9/19.
+//  Copyright © 2019 Andros Codemo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
