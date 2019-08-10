a5b8b638f71733c04b9231a643264f22317fd559
Andros Codemo <androsodemo@live.com.mx>
2019-08-09 22:19:29 -0500

    BaseNavigationController created

---
+++ AndrosCodemo CV/Modules/HomeModule/HomeView/BaseNavigationController.swift
@@ -0,0 +1,9 @@
+//
+//  BaseNavigationController.swift
+//  AndrosCodemo CV
+//
+//  Created by Andros Codemo on 8/9/19.
+//  Copyright © 2019 Andros Codemo. All rights reserved.
+//
+
+import Foundation
