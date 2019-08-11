cd956f0d2be436080b804559a403244c057c1cee
Andros Codemo <androsodemo@live.com.mx>
2019-08-10 22:58:09 -0500

    Past Experience Module development

--- AndrosCodemo CV/Modules/HomeModule/HomeBuilder/HomeModuleBuilder.swift
+++ AndrosCodemo CV/Modules/HomeModule/HomeBuilder/HomeModuleBuilder.swift
@@ -17,22 +17,7 @@ class HModuleBuilder {
         viewController.presenter = HPresenter(view      : viewController,
                                                  interactor: HInteractor(),
                                                  router    : HRouter(viewController: viewController))
-        
-        let navigationController = UINavigationController()
-        navigationController.navigationBar.tintColor     = .black
-        navigationController.navigationBar.shadowBlur    = 4
-        navigationController.navigationBar.shadowColor   = .black
-        navigationController.navigationBar.shadowOpacity = 0.4
-        navigationController.navigationBar.isTranslucent = false
-        navigationController.navigationBar.shadowOffset  = CGSize(width : 0,
-                                                                  height: 4)
-        navigationController.navigationBar.barTintColor = UIColor(red  : 225/255,
-                                                                  green: 225/255,
-                                                                  blue : 225/255,
-                                                                  alpha: 1)
-        navigationController.viewControllers = [viewController]
-        
-        return navigationController
+        return viewController
     }
     
 }
